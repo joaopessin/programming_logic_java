77f9e1a2e12cac607f9e56d8d446c97a61448cda
João V S Pessin <67722165+joaopessin@users.noreply.github.com>
2022-08-26 19:21:30 -0300

    Update MediaNotasAluno1.java

--- src/_6_exersises/MediaNotasAluno1.java
+++ src/_6_exersises/MediaNotasAluno1.java
@@ -1,9 +1,11 @@
-package _6_exersises;
+package _6_exercises;
 
 import java.util.Scanner;
 
-public class MediaNotasAluno1 {
-    static final Integer NOTA_MINIMA_PARA_PASSAR = 150;
+public class MediaNotasAluno {
+    static final Integer NOTA_MINIMA_GERAL = 150;
+    static final Integer NOTA_MINIMA = 60;
+    static final Integer NOTA_MAXIMA = 200;
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
@@ -13,16 +15,14 @@ public class MediaNotasAluno1 {
 
         System.out.print("Agora, informe a nota da prova de Matemática: ");
         float notaProvaMatematica = entrada.nextFloat();
-
-        boolean notaMinima = (notaProvaPortugues + notaProvaMatematica) >= 0;
-        boolean notaMaxima = (notaProvaPortugues + notaProvaMatematica) <= 200;
-        boolean notaMinimaEMaxima = notaMinima && notaMaxima;
+        
+        float somaNotas = notaProvaPortugues + notaProvaMatematica;
 
         System.out.print("\n");
 
-        if(notaMinimaEMaxima) {
-            boolean condicaoMinimaParaPassar = ((notaProvaPortugues + notaProvaMatematica) >= NOTA_MINIMA_PARA_PASSAR)
-                    && (notaProvaPortugues >= 60) && (notaProvaMatematica >= 60);
+        if(somaNotas >= 0 && somaNotas <= NOTA_MAXIMA) {
+            boolean condicaoMinimaParaPassar = ((notaProvaPortugues + notaProvaMatematica) >= NOTA_MINIMA_GERAL)
+                    && (notaProvaPortugues >= NOTA_MINIMA) && (notaProvaMatematica >= NOTA_MINIMA);
 
             if (condicaoMinimaParaPassar) {
                 System.out.print("Parabéns! Você conseguiu a vaga!");
