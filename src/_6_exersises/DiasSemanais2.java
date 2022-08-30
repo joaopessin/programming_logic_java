f50bcc3709c9cacc0e8bd4f74749716086457d9a
João V S Pessin <67722165+joaopessin@users.noreply.github.com>
2022-08-30 18:06:58 -0300

    Add files via upload

---
+++ src/_6_exersises/DiasSemanais2.java
@@ -0,0 +1,48 @@
+package _1_exercises;
+
+import java.util.Scanner;
+
+public class DiasSemanais2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int valorEscolhido = 0;
+
+        System.out.println("\"Dias Semanais\"");
+        System.out.print("\nInforme o valor referente ao dia da semana [1-7]: ");
+        valorEscolhido = entrada.nextInt();
+
+        String diaSemanal = "";
+
+        switch (valorEscolhido) {
+            case 1:
+                diaSemanal = "domingo";
+                break;
+            case 2:
+                diaSemanal = "segunda-feira";
+                break;
+            case 3:
+                diaSemanal = "terça-feira";
+                break;
+            case 4:
+                diaSemanal = "quarta-feira";
+                break;
+            case 5:
+                diaSemanal = "quinta-feira";
+                break;
+            case 6:
+                diaSemanal = "sexta-feira";
+                break;
+            case 7:
+                diaSemanal = "sábado";
+                break;
+            default:
+                System.err.print("Valor inválido!");
+                System.exit(1);
+        }
+
+        System.out.print("Dia da semana: " + diaSemanal);
+
+        entrada.close();
+    }
+}
