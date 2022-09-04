1c2adf35902ecc88a1a2705558f3eb2cdf3d6995
João V S Pessin <jv.pessin20@gmail.com>
2022-09-04 10:03:01 -0300

    updated NotasAluno1.java

--- src/_7_exercises/NotasAluno1.java
+++ src/_7_exercises/NotasAluno1.java
@@ -1,4 +1,31 @@
 package _7_exercises;
 
+import java.util.Scanner;
+
 public class NotasAluno1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Float notasAluno[] = new Float[] {};
+
+        System.out.print("Digite o nome do aluno: ");
+        String nomeAluno = entrada.next();
+
+        System.out.print("Agora, digite a quantidade de notas que deseja informar: ");
+        int quantidadeNotas = entrada.nextInt();
+
+        for (int i = 0; i < quantidadeNotas; i++) {
+            float nota;
+
+            System.out.print("Digite a " + (i + 1) + "ª nota: ");
+            nota = entrada.nextFloat();
+
+            notasAluno[i] = nota;
+        }
+
+        System.out.print("Notas do aluno " + nomeAluno + "\n");
+
+        for (int i = 0; i < quantidadeNotas; i++) {
+            System.out.println((i + 1) + "ª nota: " + notasAluno[i]);
+        }
+    }
 }
