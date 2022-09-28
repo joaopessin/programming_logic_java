a10bad253544b4e885aedcfa03b627795d282112
João V S Pessin <67722165+joaopessin@users.noreply.github.com>
2022-09-27 21:04:40 -0300

    Update MetodoInstancia3.java

--- src/_10_exercises/MetodoInstancia3.java
+++ src/_10_exercises/MetodoInstancia3.java
@@ -6,16 +6,16 @@ public class MetodoInstancia {
 
         cliente1.primeiroNome = "Alexandre";
         cliente1.ultimoNome = "Afonso";
-        cliente1.telefone = "34900000000";
+        cliente1.telefone = "21900000000";
         cliente1.email = "alexandre.afonso@gmail.com";
 
         Cliente cliente2 = new Cliente();
 
         cliente2.primeiroNome = "João";
         cliente2.ultimoNome = "Silva";
-        cliente2.telefone = "34899999999";
+        cliente2.telefone = "27899999999";
 
-        System.out.println("Nome do primeiro cliente: " + Cliente.obterNomeCompleto(cliente1)); // Método estático (classe)
-        System.out.println("Nome do segundo cliente: " + cliente2.obterNomeCompleto()); // Método de instancia (objeto)
+        System.out.println("Nome do primeiro cliente: " + Cliente.obterNomeCompleto(cliente1) + " - DDD: " + cliente1.obterDDD()); // Método estático (classe)
+        System.out.println("Nome do segundo cliente: " + cliente2.obterNomeCompleto() + " - DDD: " + cliente2.obterDDD()); // Método de instancia (objeto)
     }
 }
